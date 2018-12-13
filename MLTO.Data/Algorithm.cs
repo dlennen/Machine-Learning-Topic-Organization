20a895fa7f7ff471e88a44caa62f63628aa81488
dlennen <dlennen14@gmail.com>
2018-12-13 17:00:49 -0500

    first commit

--- MLTO.Data/Algorithm.cs
+++ MLTO.Data/Algorithm.cs
@@ -12,9 +12,9 @@ namespace MLTO.Data
         public Guid OwnerId { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
-  ​
-     public DateTimeOffset CreatedUtc { get; set; }
-​
-     public DateTimeOffset? ModifiedUtc { get; set; }
+        
+        public DateTimeOffset CreatedUtc { get; set; }
+        
+        public DateTimeOffset? ModifiedUtc { get; set; }
     }
 }

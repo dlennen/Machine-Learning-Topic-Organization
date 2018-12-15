dab523539b6d837ac06c043fa4df11785c86c529
dlennen <dlennen14@gmail.com>
2018-12-15 02:06:10 -0500

    preliminary MVC integration

---
+++ MLTO.Models/AlgorithmListItem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MLTO.Models
+{
+    public class AlgorithmListItem
+    {
+        [Display(Name = "ID")]
+        public int AlgorithmId { get; set; }
+
+        [Display(Name = "Algorithm")]
+        public string LearningAlgorithm { get; set; }
+
+        [Display(Name = "Mastery Level")]
+        public int MasteryLevel { get; set; }
+
+        [Display(Name = "Created")]
+        public DateTimeOffset CreatedUtc { get; set; }
+
+        [Display(Name = "Last Modification")]
+        public DateTimeOffset? ModifiedUtc { get; set; }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+}
---
+++ MachineLearningTopicOrganization.Web/Controllers/AlgorithmController.cs
@@ -0,0 +1,20 @@
+﻿using MLTO.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MachineLearningTopicOrganization.Web.Controllers
+{
+    [Authorize]
+    public class AlgorithmController : Controller
+    {
+        // GET: Algorithm
+        public ActionResult Index()
+        {
+            var model = new AlgorithmListItem[0];
+            return View(model);
+        }
+    }
+}

03be2d8878adda36f0182260d6a2f435d74ba1f2
dlennen <dlennen14@gmail.com>
2018-12-16 01:22:32 -0500

    AlgorithmService

---
+++ MLTO.Services/AlgorithmService.cs
@@ -0,0 +1,61 @@
+﻿using MLTO.Data;
+using MLTO.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MLTO.Services
+{
+    public class AlgorithmService
+    {
+        private readonly Guid _userId;
+        
+         public AlgorithmService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateAlgorithm(AlgorithmCreate model)
+        {
+            var entity =
+                new Algorithm()
+                {
+                    OwnerId = _userId,
+                    LearningAlgorithm = model.LearningAlgorithm,
+                    MasteryLevel = model.MasteryLevel,
+                    CreatedUtc = DateTimeOffset.Now
+                };
+            
+             using (var ctx = new ApplicationDbContext())
+            {
+                ctx.Algorithms.Add(entity);
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public IEnumerable<AlgorithmListItem> GetAlgorithms()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                        .Algorithms
+                        .Where(e => e.OwnerId == _userId)
+                        .Select(
+                            e =>
+                                new AlgorithmListItem
+                                {
+                                    AlgorithmId = e.AlgorithmId,
+                                    LearningAlgorithm = e.LearningAlgorithm,
+                                    MasteryLevel = e.MasteryLevel,
+                                    CreatedUtc = e.CreatedUtc
+                                }
+                        );
+                
+                 return query.ToArray();
+            }
+        }
+    }
+}
